44c34ffd4529cd341d90af4fa3f7f95f138e3ecd
zlhcsm <522496405@qq.com>
2020-04-18 07:59:10 +0800

    '统一版本'

--- wordEmbedding/main.py
+++ wordEmbedding/main.py
@@ -4,10 +4,10 @@ import seg_words as sm
 
 if __name__ == '__main__':
     # 将文件格式转换为utf8
-    gu.gbk2utf8()
+    # gu.gbk2utf8()
 
     # 分词
-    # sm.seg_words()
+    sm.seg_words()
 
     # 训练模型
     #tm.train_model()
